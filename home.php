7c07096104e607498fb81fef6df81f312c676888
Aakash Hemadri <aakashhemadri123@gmail.com>
2018-05-04 01:27:09 +0530

    Revert "Integrated login page, and few other"
    This reverts commit a901a02bd29f2375b17ec18272527f14e25bf864.

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<html>
+	<html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>The Smartphone Destination!</title>
@@ -7,26 +7,18 @@
 		<link rel="stylesheet" type="text/css" href="css/nav.css"/>
 		<link href="https://fonts.googleapis.com/css?family=Do+Hyeon|East+Sea+Dokdo" rel="stylesheet">
 		<script src="scripts/slide.js"></script>
-		<?php
-		//SETTING CURRENT USER
-			if($_POST['uid']!=NULL)
-				$username = $_POST['uid'];//After validation
-			else
-				$username = "Guest";
-		//SETTING CART DETAILS BY FETCHING DATABASE INFO
-		?>
 	</head>
 	<body onload="startTimer();">
 		<nav>
 			<div id="upperNav">
-				<a><img src="images/nav/fb.jpg" alt="search" height="17px" width="17px"></a>
-				<a><img src="images/nav/twitter.jpg" alt="search" height="17px" width="17px"></a>
-				<a><img src="images/nav/gplus.jpg" alt="search" height="13px" width="13px"></a>
-				<a><img src="images/nav/insta.jpg" alt="search" height="13px" width="13px"></a>
+				<a><img src="images/fb.jpg" alt="search" height="17px" width="17px"></a>
+				<a><img src="images/twitter.jpg" alt="search" height="17px" width="17px"></a>
+				<a><img src="images/gplus.jpg" alt="search" height="13px" width="13px"></a>
+				<a><img src="images/insta.jpg" alt="search" height="13px" width="13px"></a>
 				<div style="float:right;margin-right: 10px;">
-					<a href="pages/login.php"><img src="images/nav/login.jpg" alt="search" height="12px" width="12px" href="pages/login.html"></a>
-					<a href="pages/cart.php"><img src="images/nav/cart.jpg" alt="search" height="14" width="14px"></a>
-					<a><img src="images/nav/search.jpg" alt="search" height="12px" width="12px"></a>
+					<a><img src="images/login.jpg" alt="search" height="12px" width="12px" href="pages/login.html"></a>
+					<a><img src="images/cart.jpg" alt="search" height="14" width="14px"></a>
+					<a><img src="images/search.jpg" alt="search" height="12px" width="12px"></a>
 					<input type="text" name="" placeholder="Product name">
 				</div>
 			</div>
@@ -43,7 +35,7 @@
 				<div style="float:right;margin-right: 10px;">
 					<a href="pContact">CONTACT</a>
 					<a href="pService">SERVICE</a>
-					<a href="pages/about.php">ABOUT</a>
+					<a href="pages\about.html">ABOUT</a>
 				</div>
 			</div>
 		</nav>
@@ -60,8 +52,11 @@
 		<div id="#pPremium">
 			
 		</div>
-		<?php
-			include 'php/footer.php';
-		?>
+		<footer>
+			<p>
+				Owned and Founded by gadgets4you<br>
+				&copy;2018 gadgets4you.com<br>
+			</p>
+		</footer>
 	</body>
 </html>
--- php/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-		<p>
-			<?php
-			echo "While using this site you have agreed to have read and accepted the terms of use, cookie and <a href='../pages/pp.html'>privacy policy</a>. <br>Copyrights 2000 - ". date('Y') . " by gadgets4you.com <br>";
-			?>
-		</p>
-</footer>
