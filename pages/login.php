39cb3406d8b86bb8f26a776238d626bd008436c5
Aakash Hemadri <aakashhemadri123@gmail.com>
2018-05-05 22:55:41 +0530

    Added support for login that starts a session
    Now we need to add cart data support , featured products and product page!

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,71 @@
+<?php
+  require '../php/connect.php';
+
+  $username = $password = "";
+  $username_err = $password_err = "";
+
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+      $username_err = "Please enter a username";
+    }
+    else{
+      $username = $_POST["username"];
+    }
+
+    // Validating password
+    if(empty(trim($_POST["password"]))){
+      $password_err = "Please enter a password";
+    }
+    else{
+      $password = $_POST["password"];
+    }
+
+    //Check for input errors before validating login!
+    if(empty($username_err) && empty($password_err)){
+      //Prepare mysqli statement
+      $sql = "SELECT username, password FROM users WHERE username = ?";
+      if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            // Set parameters
+            $param_username = $username;
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+            // Store result
+              mysqli_stmt_store_result($stmt);    
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            /* Password is correct, so start a new session and save the username to the session */
+                            session_start();
+                            $_SESSION['username'] = $username;      
+                            header("location: ../home.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = 'The password you entered was not valid.';
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = 'No account found with that username.';
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+// Close connection
+mysqli_close($link);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
